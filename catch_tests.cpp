9230aad1496fe417098b0e1d096ccbfa34b629e3
Ricardo Abreu <ricab@ricabhome.org>
2018-02-16 02:49:15 +0000

    add plain function test

--- catch_tests.cpp
+++ catch_tests.cpp
@@ -3,3 +3,27 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main()
 #include "catch/catch.hpp"
 
+using namespace sg;
+
+namespace
+{
+  //////////////////////////////////////////////////////////////////////////////
+  bool f_called = false;
+
+  //////////////////////////////////////////////////////////////////////////////
+  void f()
+  {
+    f_called = true;
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+TEST_CASE("Plain function")
+{
+  {
+    REQUIRE_FALSE(f_called);
+    auto guard = make_scope_guard(f);
+    REQUIRE_FALSE(f_called);
+  }
+  REQUIRE(f_called);
+}
