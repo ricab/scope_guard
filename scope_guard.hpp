ea44007197c39d175b13f79a03ef6d33d63ab0e7
Ricardo Abreu <ricab@ricabhome.org>
2018-02-16 18:29:07 +0000

    change order of scope_guard attrs - feels more natural

--- scope_guard.hpp
+++ scope_guard.hpp
@@ -28,8 +28,8 @@ namespace sg
     ~scope_guard();
 
   private:
-    bool m_active;
     Callable m_resetter;
+    bool m_active;
   };
 
   /// helper to create scope_guard and deduce template params
@@ -41,8 +41,8 @@ namespace sg
 template<typename Callable>
 template<typename>
 sg::scope_guard<Callable>::scope_guard(Callable&& resetter)
-  : m_active{true}
-  , m_resetter{std::forward<Callable>(resetter)}
+  : m_resetter{std::forward<Callable>(resetter)}
+  , m_active{true}
 {}
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -56,8 +56,8 @@ sg::scope_guard<Callable>::~scope_guard()
 ////////////////////////////////////////////////////////////////////////////////
 template<typename Callable>
 sg::scope_guard<Callable>::scope_guard(scope_guard&& other)
-  : m_active{other.m_active}
-  , m_resetter{std::move(other.m_resetter)}
+  : m_resetter{std::move(other.m_resetter)}
+  , m_active{std::move(other.m_active)}
 {
   other.m_active = false;
 }
