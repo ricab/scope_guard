313108b4bd9de2cf6ecfe1468f377223c10f93f4
Ricardo Abreu <ricab@ricabhome.org>
2018-02-21 01:59:12 +0000

    move scope_guard into detail namespace

--- scope_guard.hpp
+++ scope_guard.hpp
@@ -13,43 +13,48 @@
 ////////////////////////////////////////////////////////////////////////////////
 namespace sg
 {
-  /**
-   * RAII class to call a resetter function when leaving scope. The resetter
-   * needs to be compatible with std::function<void()>.
-   */
-  template<typename Callback>
-  class scope_guard // TODO put into detail namespace
+  namespace detail
   {
-  public:
-    template<typename = typename std::enable_if<
-      std::is_constructible<std::function<void()>, Callback>::value>::type>
-    explicit scope_guard(Callback&& callback);
+    /**
+     * RAII class to call a resetter function when leaving scope. The resetter
+     * needs to be compatible with std::function<void()>.
+     */
+    template<typename Callback>
+    class scope_guard
+    {
+    public:
+      template<typename = typename std::enable_if<
+        std::is_constructible<std::function<void()>, Callback>::value>::type>
+      explicit scope_guard(Callback&& callback);
 
-    scope_guard(scope_guard&& other);
-    ~scope_guard();
+      scope_guard(scope_guard&& other);
+      ~scope_guard();
 
-  private:
-    Callback m_callback;
-    bool m_active;
+    private:
+      Callback m_callback;
+      bool m_active;
 
-  };
+    };
+
+  } // namespace detail
 
   /// helper to create scope_guard and deduce template params
   template<typename Callback>
-  scope_guard<Callback> make_scope_guard(Callback&& resetter);
-}
+  detail::scope_guard<Callback> make_scope_guard(Callback&& resetter);
+
+} // namespace sg
 
 ////////////////////////////////////////////////////////////////////////////////
 template<typename Callback>
 template<typename>
-sg::scope_guard<Callback>::scope_guard(Callback&& callback)
+sg::detail::scope_guard<Callback>::scope_guard(Callback&& callback)
   : m_callback{std::forward<Callback>(callback)}
   , m_active{true}
 {}
 
 ////////////////////////////////////////////////////////////////////////////////
 template<typename Callback>
-sg::scope_guard<Callback>::~scope_guard()
+sg::detail::scope_guard<Callback>::~scope_guard()
 {
   if(m_active)
     m_callback();
@@ -57,7 +62,7 @@ sg::scope_guard<Callback>::~scope_guard()
 
 ////////////////////////////////////////////////////////////////////////////////
 template<typename Callback>
-sg::scope_guard<Callback>::scope_guard(scope_guard&& other)
+sg::detail::scope_guard<Callback>::scope_guard(scope_guard&& other)
   : m_callback{std::forward<Callback>(other.m_callback)}
   , m_active{std::move(other.m_active)}
 {
@@ -66,9 +71,10 @@ sg::scope_guard<Callback>::scope_guard(scope_guard&& other)
 
 ////////////////////////////////////////////////////////////////////////////////
 template<typename Callback>
-inline auto sg::make_scope_guard(Callback&& resetter) -> scope_guard<Callback>
+inline auto sg::make_scope_guard(Callback&& resetter)
+-> detail::scope_guard<Callback>
 {
-  return scope_guard<Callback>{std::forward<Callback>(resetter)};
+  return detail::scope_guard<Callback>{std::forward<Callback>(resetter)};
 }
 
 #endif /* SCOPE_GUARD_HPP_ */
