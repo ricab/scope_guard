ccd0807a55a710e5111b5e88eed7a113d1fa5f5f
Ricardo Abreu <ricab@ricabhome.org>
2018-03-08 22:41:17 +0000

    add compile time tests to check noexcept ctor and maker; refactor cmake code to have only one loop over the test range

--- compile_time_tests.cpp
+++ compile_time_tests.cpp
@@ -58,6 +58,16 @@ namespace
 #endif
   }
 
+#ifdef test_4
+  static_assert(noexcept(make_scope_guard(non_throwing)),
+                "make_scope_guard not noexcept");
+#endif
+
+#ifdef test_5
+  static_assert(noexcept(detail::scope_guard<void(*)()noexcept>{non_throwing}),
+                "scope_guard ctor not noexcept");
+#endif
+
   /**
    * Highlight that scope_guard should not be created with throwing callables,
    * under penalty of an immediate std::terminate call. Test that compilation
@@ -65,19 +75,19 @@ namespace
    */
   void test_noexcept_bad()
   {
-#ifdef test_4
+#ifdef test_6
     make_scope_guard(throwing);
 #endif
-#ifdef test_5
+#ifdef test_7
     make_scope_guard(throwing_stdfun);
 #endif
-#ifdef test_6
+#ifdef test_8
     make_scope_guard(throwing_lambda);
 #endif
-#ifdef test_7
+#ifdef test_9
     make_scope_guard(throwing_bound);
 #endif
-#ifdef test_8
+#ifdef test_10
     make_scope_guard(throwing_functor);
 #endif
   }
@@ -89,19 +99,19 @@ namespace
    */
   void test_noexcept_fixable()
   {
-#ifdef test_9
+#ifdef test_11
     make_scope_guard(meh);
 #endif
-#ifdef test_10
+#ifdef test_12
     make_scope_guard(meh_stdfun);
 #endif
-#ifdef test_11
+#ifdef test_13
     make_scope_guard(meh_lambda);
 #endif
-#ifdef test_12
+#ifdef test_14
     make_scope_guard(meh_bound);
 #endif
-#ifdef test_13
+#ifdef test_15
     make_scope_guard(meh_functor);
 #endif
   }
@@ -114,10 +124,10 @@ namespace
    */
   void test_noexcept_unfortunate()
   {
-#ifdef test_14
+#ifdef test_16
     make_scope_guard(non_throwing_stdfun);
 #endif
-#ifdef test_15
+#ifdef test_17
     make_scope_guard(non_throwing_bound);
 #endif
   }
@@ -128,7 +138,7 @@ namespace
   void test_disallowed_copy_construction()
   {
     const auto guard = make_scope_guard(non_throwing);
-#ifdef test_16
+#ifdef test_18
     const auto guard2 = guard1;
 #endif
   }
@@ -140,7 +150,7 @@ namespace
   {
     const auto guard1 = make_scope_guard(non_throwing_lambda);
     auto guard2 = make_scope_guard(non_throwing_functor);
-#ifdef test_17
+#ifdef test_19
     guard2 = guard1;
 #endif
   }
@@ -151,11 +161,12 @@ namespace
   void test_disallowed_move_assignment()
   {
     auto guard = make_scope_guard(non_throwing);
-#ifdef test_18
+#ifdef test_20
     guard = make_scope_guard(non_throwing_lambda);
 #endif
   }
 }
+
 
 int main()
 {
